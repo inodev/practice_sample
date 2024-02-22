280a42ac4fa0518a2e4be86f34e09f5d97c9a063
inoue.shinya <inosdevelopment@gmail.com>
2024-02-22 23:29:15 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+comics = [
+  {
+    id: 1,
+    title: '葬送のフリーレン',
+    summary: '『葬送のフリーレン』（そうそうのフリーレン）は、山田鐘人（原作）、アベツカサ（作画）による日本の漫画。『週刊少年サンデー』（小学館）にて、2020年22・23合併号より連載中'
+  },
+  {
+    id: 2,
+    title: 'ワールドトリガー',
+    summary: '『ワールドトリガー』（WORLD TRIGGER）は、葦原大介による日本の漫画作品。『週刊少年ジャンプ』（集英社）にて2013年11号から2018年52号まで連載された後、『ジャンプSQ.』にて2019年1月号から連載中'
+  },
+  {
+    id: 3,
+    title: '瀬戸の花嫁',
+    summary: '『瀬戸の花嫁』（せとのはなよめ、My Bride Is a Mermaid）は、木村太彦による同名の漫画を原作としたテレビアニメ作品。'
+  }
+]
+
+Comic.insert_all(comics)
