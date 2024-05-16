205341cccbd1683cd17fe76d701d348450947256
oh84 <oh84.jp@gmail.com>
2024-05-16 23:06:03 +0900

    認証の仕組みを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+  private
+
+  def current_user
+    return @current_user if defined?(@current_user)
+
+    authorization_header = request.headers[:authorization]
+    return @current_user = nil if authorization_header.blank?
+
+    token = authorization_header.split(' ')[1]
+    secret_key = Rails.application.secret_key_base
+    decoded_token = JWT.decode(token, secret_key)
+    user_id = decoded_token[0]['user_id']
+    # @current_user = User.find_by(id: user_id)
+    @current_user = 'user'
+
+  rescue JWT::DecodeError
+    @current_user = nil
+  end
+
+  def authenticate_user!
+    head :unauthorized if current_user.nil?
+  end
 end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,7 @@
 class HomesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    render json: { message: "Hello, World!" }
+    render json: { message: current_user }
   end
 end
